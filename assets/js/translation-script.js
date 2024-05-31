558463d31852f6246a4adf524a5fdb08f896c5af
Sofia Alfaro <134465624+asofialf@users.noreply.github.com>
2024-05-31 12:08:30 -0500

    update: translation script

--- assets/js/translation-script.js
+++ assets/js/translation-script.js
@@ -59,6 +59,41 @@ function applyTranslations() {
   replaceText("terms_conditions", "terms_conditions");
   replaceText("legal", "legal");
   replaceText("connect_with_us", "connect_with_us");
+  replaceText("pricing-company-title", "pricing-company-title");
+  replaceText("pricing-company-subtitle", "pricing-company-subtitle");
+  replaceText("plan-basic", "plan-basic");
+  replaceText("plan-basic-price", "plan-basic-price");
+  replaceText("basic-functionalities", "basic-functionalities");
+  replaceText("weekly-statistics", "weekly-statistics");
+  replaceText("1-user", "1-user");
+  replaceText("limited-fleet-registration", "limited-fleet-registration");
+  replaceText("no-priority-support", "no-priority-support");
+  replaceText("pricing-button", "pricing-button");
+  replaceText("plan-most-popular", "plan-most-popular");
+  replaceText("plan-pro", "plan-pro");
+  replaceText("plan-pro-price", "plan-pro-price");
+  replaceText("complete-functionalities", "complete-functionalities");
+  replaceText("annual-statistics", "annual-statistics");
+  replaceText("5-users", "5-users");
+  replaceText("unlimited-fleet-registration", "unlimited-fleet-registration");
+  replaceText("email-support", "email-support");
+  replaceText("plan-enterprice", "plan-enterprice");
+  replaceText("plan-enterprice-price", "plan-enterprice-price");
+  replaceText("unlimited-users", "unlimited-users");
+  replaceText("24-7-email-support", "24-7-email-support");
+  replaceText("pricing-passenger-title", "pricing-passenger-title");
+  replaceText("pricing-passenger-subtitle", "pricing-passenger-subtitle");
+  replaceText("plan-basic-passenger-price", "plan-basic-passenger-price");
+  replaceText("trip-planner", "trip-planner");
+  replaceText("line-schedule", "line-schedule");
+  replaceText("time-location", "time-location");
+  replaceText("next-arrival", "next-arrival");
+  replaceText("ads", "ads");
+  replaceText("plan-pro-passenger-price", "plan-pro-passenger-price");
+  replaceText("next-arrivals", "next-arrivals");
+  replaceText("arrival-updates", "arrival-updates");
+  replaceText("route-progress", "route-progress");
+  replaceText("ad-free", "ad-free");
 }
 
 languageSelector.addEventListener("change", function () {
